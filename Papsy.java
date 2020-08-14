39a9499e52d487b4a31015896393b1f24c5203e9
Papsy <57874831+uxp123@users.noreply.github.com>
2020-08-14 19:25:57 +0000

    Add files via upload

---
+++ Papsy.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package papsy;
+
+/**
+ *
+ * @author philipfynn
+ */
+public class Papsy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.name = "John";
+        s.roll_no = 2;
+        System.out.println("His name is " + s.name + " and roll number is " + s.roll_no);
+        // TODO code application logic here
+    }
+    
+}
